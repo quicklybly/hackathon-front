fe3b6732077e81c10f8f87593e9dc876a9bec94a
elena.sheina <lena_sheina@bk.ru>
2023-04-27 23:11:02 +0300

    Design auth

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -2,13 +2,13 @@
     <div class="header">
         <div class="container text-center">
             <div class="row">
-                <div class="col-2">
+                <div class="col-lg-2 col-md-2 col-xs-12" style="margin: auto">
                     <img
                         @click="$router.push('/')"
                         src="./img/ФСП_Короткий1.png"
                         class="img-fluid clickable">
                 </div>
-                <div class="col-2">
+                <div class="col-lg-2 col-md-2 col-xs-12" style="margin: auto">
                     <button
                         @click="$router.push('/news')"
                         type="button"
@@ -16,7 +16,7 @@
                         Новости
                     </button>
                 </div>
-                <div class="col-2">
+                <div class="col-lg-2 col-md-2 col-xs-12" style="margin: auto">
                     <button
                         @click="$router.push('/test')"
                         type="button"
@@ -24,7 +24,7 @@
                         Календарь
                     </button>
                 </div>
-                <div class="col-2">
+                <div class="col-lg-2 col-md-2 col-xs-12" style="margin: auto">
                     <button
                         @click="$router.push('/about')"
                         type="button"
@@ -32,8 +32,8 @@
                         Контакты
                     </button>
                 </div>
-                <div class="col-2"></div>
-                <div class="col-2">
+                <div class="col-lg-2 col-md-1 col-xs-12" style="margin: auto"></div>
+                <div class="col-lg-2 col-md-3 col-xs-12" style="margin: auto">
                     <button
                         @click="$router.push('/auth')"
                         type="button"
--- src/views/AuthPage.vue
+++ src/views/AuthPage.vue
@@ -1,55 +1,113 @@
-<template>
-    <div
-            v-if="!isRecovery && !isRegistration"
-            class="auth">
-        <h2>Вход в аккаунт</h2>
-        <dnlkk-input
-                placeholder="Ваш логин"/>
-        <dnlkk-input
-                type="password"
-                placeholder="Ваш пароль"/>
-        <i @click="onChangeRecovery" style="cursor: pointer">Забыли
-            пароль?</i>
-        <i @click="onChangeRegistration" style="cursor:
-        pointer">Ещё не зарегистрированы?</i>
+<template xmlns="http://www.w3.org/1999/html">
+  <div class="content">
+  <div v-if="!isRecovery && !isRegistration">
+    <div class ="container text-center craft-container" style="display: flex;
+                    flex-direction: row;
+                    align-self: center;
+                    justify-content: center;">
+      <div class="col-lg-2"></div>
+      <div class="col-lg-6" style = "border: solid 2px #EC1D35; border-radius: 5px; padding: 20px 30px 20px; margin: auto;" >
+        <div class="row">
+          <div v-if="!isRecovery && !isRegistration"
+                  class="auth">
+              <p class="title">Вход в аккаунт</p>
+          </div>
+        </div>
+        <div class="row">
+          <dnlkk-input placeholder="Ваш логин"/>
+        </div>
+        <div class="row">
+          <dnlkk-input
+                  type="password"
+                  placeholder="Ваш пароль"/>
+        </div>
+        <div class = "row">
+          <i @click="onChangeRecovery" style="cursor: pointer">Забыли
+              пароль?</i>
+          <i @click="onChangeRegistration" style="cursor:
+        pointer">Зарегистрироваться</i>
+        </div>
+      </div>
+      <div class = "col-lg-2"></div>
     </div>
+  </div>
     <div
             v-if="isRecovery"
-            class="recovery">
-        <h2>Восстановление пароля</h2>
+            class="recovery container text-center craft-container"
+            >
+      <div class="col-2"></div>
+      <div class="col-8" style = "border: solid 2px #EC1D35; border-radius: 5px; padding: 20px 30px 20px; margin: auto;">
+      <div class="row">
+        <p>Восстановление пароля</p>
+      </div>
+      <div class="row">
         <dnlkk-input
                 placeholder="Ваша почта/логин"/>
+      </div>
+      <div class="row">
         <i @click="onChangeRecovery" style="cursor: pointer">Я
             вспомнил пароль</i>
         <i @click="onChangeRegistration" style="cursor:
         pointer">Я вспомнил что ещё не зарегистрирован!</i>
+      </div>
+      </div>
+      <div class="col-2"></div>
     </div>
-    <div
-        v-if="isRegistration"
-        class="reg">
-        <dnlkk-input
-            placeholder="Логин"/>
-        <dnlkk-input
-            type="password"
-            placeholder="Пароль"/>
-        <dnlkk-input
-            type="password"
-            placeholder="Повторите пароль"/>
-        <dnlkk-input
-            placeholder="Почта"/>
-        <dnlkk-input
-            placeholder="Имя"/>
-        <dnlkk-input
-            placeholder="Фамилия"/>
-        <dnlkk-input
-            placeholder="Отчество"/>
-        <dnlkk-input
-            type="date"
-            placeholder="Дата рождения"/>
-        <i @click="onChangeRegistration" style="cursor: pointer">Я
-            вспомнил, что уже зарегистрован!</i>
-
+  <div v-if="isRegistration"
+       class="container text-center craft-container"
+       style=" display: flex;
+                justify-content: center;">
+    <div class="col-3"></div>
+    <div class="col-6" style = "border: solid 2px #EC1D35; border-radius: 5px;">
+      <div
+          class="reg" style = "padding: 20px 30px 20px; margin: auto;">
+        <div class="row">
+          <p>Регистрация</p>
+        </div>
+        <div class="row">
+          <dnlkk-input
+              placeholder="Логин"/>
+        </div>
+        <div class="row">
+          <dnlkk-input
+              type="password"
+              placeholder="Пароль"/>
+        </div>
+        <div class="row">
+          <dnlkk-input
+              type="password"
+              placeholder="Повторите пароль"/>
+        </div>
+        <div class="row">
+          <dnlkk-input
+              placeholder="Почта"/>
+        </div>
+        <div class="row">
+          <dnlkk-input
+              placeholder="Имя"/>
+        </div>
+        <div class="row">
+          <dnlkk-input
+              placeholder="Фамилия"/>
+        </div>
+        <div class="row">
+          <dnlkk-input
+              placeholder="Отчество"/>
+        </div>
+        <div class="row">
+          <dnlkk-input
+              type="date"
+              placeholder="Дата рождения"/>
+        </div>
+        <div class="row row1">
+          <i @click="onChangeRegistration" style="cursor: pointer">Я
+              вспомнил, что уже зарегистрован!</i>
+        </div>
+      </div>
     </div>
+    <div class="col-3"></div>
+  </div>
+  </div>
 </template>
 
 <script>
@@ -76,4 +134,21 @@ export default {
 </script>
 
 <style scoped>
+.content {
+  background-color: #1B1C21;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+}
+i, P {
+  color: #EDEDED;
+  font-weight: 500;
+}
+.row1 {
+  padding-top: 10px;
+}
+.craft-container {
+  margin-top: 5%;
+}
+
 </style>

b438abe987a95f459801f3b298abebc7de1d4ed7
danilkk <ezzfvkoko@gmail.com>
2023-04-28 01:10:57 +0300

    add modal window

---
+++ src/components/UI/DnlkkDialog.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class="dialog" v-if="show"
+         @click.stop="hideDialog">
+        <div @click.stop class="dialog__content">
+            <slot></slot>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "dnlkk-dialog",
+    props: {
+        show: {
+            type: Boolean,
+            default: false
+        }
+    },
+    methods: {
+        hideDialog() {
+            this.$emit('update:show', false)
+        }
+    }
+}
+</script>
+
+<style scoped>
+.dialog {
+	top: 0;
+	bottom: 0;
+	right: 0;
+	left: 0;
+	z-index: 1;
+	background: rgba(0, 0, 0, 0.5);
+	position: fixed;
+	display: flex;
+}
+
+.dialog__content {
+	margin: auto;
+    border-radius: 12px;
+    min-height: 50px;
+	min-width: 300px;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,23 +1,37 @@
 <template>
     <div class="home">
-        <dnlkk-button class="info">hi there
+        <dnlkk-button
+            @click="showDialog"
+            class="info">hi there
             !</dnlkk-button>
         <dnlkk-input v-focus
                      v-mode="title"
                     placeholder="hi there!"/>
+<!--        <dnlkk-dialog-->
+<!--            v-model:show="dialogVisible">-->
+<!--        <sign-in-page/>-->
+<!--        </dnlkk-dialog>-->
     </div>
 </template>
 
 <script>
 // @ is an alias to /src
 
+import SignInPage from "@/views/AuthPage.vue";
+
 export default {
     name: 'HomeView',
-    components: {},
+    components: {SignInPage},
     data() {
         return {
-            title: ''
+            title: '',
+            dialogVisible: false,
         }
+    },
+    methods: {
+        showDialog() {
+            this.dialogVisible = true
+        },
     }
 
 }

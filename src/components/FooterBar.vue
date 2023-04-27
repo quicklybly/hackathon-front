e1c2e673b02c65caaa2f7ff429f41ae2b0bbdddb
danilkk <ezzfvkoko@gmail.com>
2023-04-27 11:29:50 +0300

    Update header and footer

---
+++ src/components/FooterBar.vue
@@ -0,0 +1,38 @@
+<template>
+  <link rel="stylesheet" href="fontello-a6d9d753/css/fontello.css">
+  <div class = "footer container text-center">
+    <div class="row">
+      <div class="col-6"><img src = "./img/dark_background1.png" class="img-fluid"></div>
+      <div class="col-4"></div>
+      <div class="col-2 itd_circles">
+        <div class="itd_circle"><i class = "icon-vkontakte"></i></div>
+        <div class="itd_circle"><i class = "icon-youtube-play"></i></div>
+        <div class="itd_circle"><i class = "icon-telegram"></i></div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+    name: "FooterBar"
+}
+</script>
+
+<style scoped>
+.footer {
+  background-color: #1B1C21;
+}
+.itd_circle {
+  margin: 0 auto 15px;
+  width: 50px;
+  height: 50px;
+  border: 3px solid #3A3C46;
+  border-radius: 50%;
+}
+.itd_circles {
+  padding-top: 15px;
+  display: flex;
+}
+
+</style>
--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,32 +1,39 @@
 <template>
-    <div class="navbar">
-        <div @click="$router.push('/')">
-            <h2 style="color: white;">logo placeholder</h2>
-        </div>
-        <div>
-            <dnlkk-button
-                    @click="$router.push('/sign-in')">
-                Войти
-            </dnlkk-button>
-        </div>
+    <div class="header container text-center">
+      <div class ="row">
+        <div class = "col-2"><img src = "./img/ФСП_Короткий1.png" class="img-fluid"></div>
+        <div class = "col-2"><button type="button" class="btn btn-itm-color btn-lg">Событие</button></div>
+        <div class = "col-2"><button type="button" class="btn btn-itm-color btn-lg">Календарь</button></div>
+        <div class = "col-2"><button type="button" class="btn btn-itm-color btn-lg">Контакты</button></div>
+        <div class = "col-2"></div>
+        <div class = "col-2"><button type="button" class="btn btn-itm-color btn-lg">войти</button></div>
+      </div>
     </div>
 </template>
 
 <script>
 
+import DnlkkButton from "@/components/UI/DnlkkButton";
 export default {
     name: "NavBar",
+  components: {DnlkkButton},
 }
 </script>
 
 <style scoped>
-.navbar {
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    background: teal;
-    padding: 10px;
-
+:root {
+  --color1: #1B1C21; /*black*/
+  --color2: #2D2E37; /*gray bright*/
+  --color3: #3A3C46; /*gray middle*/
+  --color4: #9B9C9E; /*gray lighte-2*/
+  --color5: #D9D9D9; /*gray lighte*/
+  --color6: #EDEDED; /*white*/
+}
+.header {
+  background-color: #1B1C21;
 }
 
+.btn-itm-color {
+  color: #EDEDED;
+}
 </style>

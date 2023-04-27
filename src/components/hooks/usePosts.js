db003a29a3fe62745bc92f85f13da1589ca3df6c
danilkk <ezzfvkoko@gmail.com>
2023-04-27 19:35:16 +0300

    mixins bye-bye!

--- src/components/hooks/usePosts.js
+++ src/components/hooks/usePosts.js
@@ -1,25 +1,9 @@
 import {ref} from "vue";
 
-export default function usePosts() {
+export default function usePosts(props) {
     const posts = ref([])
 
-    posts.value.push([{
-        title: 'test1',
-        body: 'hi there!',
-        rating: -22,
-        tags: ['c', 'crips', 'a lot of food', 'middle'],
-        filter: 'CTF'},
-        {title: 'test2',
-        body: 'F:LSAJOPFJASOFJASPIHJFPOQWKFPOASJPOOPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPP!',
-        rating: 100,
-        tags: ['flask', 'spring', 'summer', 'avangard'],
-        filter: 'hackathon'},
-        {title: 'test3',
-        body: 'hi there!',
-        rating: -9999,
-        tags: ['fanta', 'cola', 'senior', 'sprite', 'google'],
-        filter: 'algorithmic'},
-    ])
+    posts.value.push(props['posts'])
 
     console.log(posts)
 

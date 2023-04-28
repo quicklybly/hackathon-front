72f02b24da42a89d6cd6dd26d3ad59eae3e28b24
danilkk <ezzfvkoko@gmail.com>
2023-04-28 08:51:57 +0300

    auth + checks for auth&reg

---
+++ src/components/Registration.vue
@@ -0,0 +1,124 @@
+<template>
+    <div
+            class="container text-center craft-container"
+            style=" display: flex;
+                justify-content: center;">
+        <div class="col-3"></div>
+        <div class="col-6 form">
+            <div
+                    class="reg"
+                    style="padding: 20px 30px 20px; margin: auto;">
+                <div class="row">
+                    <p>Зарегистрироваться в ФСП</p>
+                    <p v-if="isError"
+                       style="color: indianred">Ой! В одном из
+                        полей ошибка.</p>
+                </div>
+                <div class="row">
+                    <dnlkk-input
+                        v-model="login"
+                            placeholder="Логин" required/>
+                </div>
+                <div class="row">
+                    <dnlkk-input
+                        v-model="password"
+                            type="password"
+                            placeholder="Пароль" required/>
+                </div>
+                <div class="row">
+                    <dnlkk-input
+                        v-model="repeatPass"
+                            type="password"
+                            placeholder="Повторите пароль" required/>
+                </div>
+                <div class="row">
+                    <dnlkk-input
+                        v-model="email"
+                            placeholder="Почта" required/>
+                </div>
+                <div class="row">
+                    <dnlkk-input
+                        v-model="name"
+                            placeholder="Имя" required/>
+                </div>
+                <div class="row">
+                    <dnlkk-input
+                        v-model="surname"
+                            placeholder="Фамилия" required/>
+                </div>
+                <div class="row">
+                    <dnlkk-input
+                        v-model="patronymic"
+                            placeholder="Отчество"/>
+                </div>
+                <div class="row">
+                    <dnlkk-input
+                        v-model="birthday"
+                            type="date"
+                            placeholder="Дата рождения"
+                            required/>
+                </div>
+                <div class="row btn-row">
+                    <button
+                        @click="reg"
+                        type="button"
+                            class="btn btn-craft btn-lg btn-outline-danger">
+                        Регистрирация
+                    </button>
+                </div>
+                <div class="row row1">
+                    <i class="btn-hover-link"
+                       @click="onChangeRegistration"
+                       style="cursor: pointer">Я
+                        вспомнил, что уже зарегистрован!</i>
+                </div>
+            </div>
+        </div>
+        <div class="col-3"></div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Registration",
+    data(){
+        return{
+            login: '',
+            password: '',
+            repeatPass: '',
+            email: '',
+            name: '',
+            surname: '',
+            patronymic: '',
+            birthday: '',
+            isError: false
+        }
+    },
+    methods: {
+        onChangeRegistration() {
+            this.$emit('reg')
+        },
+        reg() {
+            if (this.login.length > 0 &&
+                this.password.length > 0 &&
+                this.email.includes('@') &&
+                this.password === this.repeatPass &&
+                this.email.length > 0 &&
+                this.name.length > 0 &&
+                this.surname.length > 0 &&
+                this.birthday.length > 0){
+                    this.isError = false
+                    this.postRegistration()
+            } else {
+                this.isError = true
+            }
+        },
+        async postRegistration() {
+
+        }
+    }
+}
+</script>
+
+<style>
+</style>
--- src/views/AuthPage.vue
+++ src/views/AuthPage.vue
@@ -1,140 +1,31 @@
 <template xmlns="http://www.w3.org/1999/html">
   <div class="content">
-  <div v-if="!isRecovery && !isRegistration">
-    <div class ="container text-center craft-container" style="display: flex;
-                    flex-direction: row;
-                    align-self: center;
-                    justify-content: center;">
-      <div class="col-lg-2"></div>
-      <div class="col-lg-6 form" >
-        <div class="row">
-          <div
-                  class="auth">
-              <p class="title">Вход в аккаунт</p>
-          </div>
-        </div>
-        <div class="row">
-          <dnlkk-input v-model="login"
-                       placeholder="Ваш логин"/>
-        </div>
-        <div class="row">
-          <dnlkk-input
-                  v-model="password"
-                  type="password"
-                  placeholder="Ваш пароль"/>
-        </div>
-        <div class="row btn-row">
-          <dnlkk-button type="button"
-                        class="btn btn-craft btn-lg btn-outline-danger"
-          @click="onAuth(login, password)">Войти</dnlkk-button>
-        </div>
-        <div class = "row">
-          <i class="btn-hover-link"
-             @click="onChangeRecovery"
-             style="cursor: pointer">
-              Забыли
-              пароль?</i>
-          <i class="btn-hover-link" 
-             @click="onChangeRegistration" style="cursor:
-        pointer">Зарегистрироваться</i>
-        </div>
-      </div>
-      <div class = "col-lg-2"></div>
-    </div>
-  </div>
-    <div
+    <auth
+          @reg="onChangeRegistration"
+          @recovery="onChangeRecovery"
+          v-if="!isRecovery && !isRegistration"/>
+    <recovery
+            @reg="onChangeRegistration"
+            @recovery="onChangeRecovery"
             v-if="isRecovery"
-            class="recovery container text-center craft-container"
-            >
-      <div class="col-2"></div>
-      <div class="col-8 form">
-      <div class="row">
-        <p>Восстановление пароля</p>
-      </div>
-      <div class="row">
-        <dnlkk-input
-                placeholder="Ваша почта/логин"/>
-      </div>
-      <div class="row btn-row">
-        <button type="button" class="btn btn-craft btn-lg btn-outline-danger">Восстановить</button>
-      </div>
-      <div class="row">
-        <i class="btn-hover-link"
-           @click="onChangeRecovery" style="cursor: pointer">Я
-            вспомнил пароль</i>
-        <i class="btn-hover-link"
-           @click="onChangeRegistration" style="cursor:
-        pointer">Я вспомнил что ещё не зарегистрирован!</i>
-      </div>
-      </div>
-      <div class="col-2"></div>
-    </div>
-  <div v-if="isRegistration"
-       class="container text-center craft-container"
-       style=" display: flex;
-                justify-content: center;">
-    <div class="col-3"></div>
-    <div class="col-6 form">
-      <div
-          class="reg" style = "padding: 20px 30px 20px; margin: auto;">
-        <div class="row">
-          <p>Зарегистрироваться в ФСП</p>
-        </div>
-        <div class="row">
-          <dnlkk-input
-              placeholder="Логин"/>
-        </div>
-        <div class="row">
-          <dnlkk-input
-              type="password"
-              placeholder="Пароль"/>
-        </div>
-        <div class="row">
-          <dnlkk-input
-              type="password"
-              placeholder="Повторите пароль"/>
-        </div>
-        <div class="row">
-          <dnlkk-input
-              placeholder="Почта"/>
-        </div>
-        <div class="row">
-          <dnlkk-input
-              placeholder="Имя"/>
-        </div>
-        <div class="row">
-          <dnlkk-input
-              placeholder="Фамилия"/>
-        </div>
-        <div class="row">
-          <dnlkk-input
-              placeholder="Отчество"/>
-        </div>
-        <div class="row">
-          <dnlkk-input
-              type="date"
-              placeholder="Дата рождения"/>
-        </div>
-        <div class="row btn-row">
-          <button type="button" class="btn btn-craft btn-lg btn-outline-danger">Регистрирация</button>
-        </div>
-        <div class="row row1">
-          <i class="btn-hover-link"
-             @click="onChangeRegistration" style="cursor: pointer">Я
-              вспомнил, что уже зарегистрован!</i>
-        </div>
-      </div>
-    </div>
-    <div class="col-3"></div>
-  </div>
+            />
+  <registration v-if="isRegistration"
+                @reg="onChangeRegistration"/>
   </div>
 </template>
 
 <script>
 
 
+import Auth from "@/components/Auth.vue";
+import Recovery from "@/components/Recovery.vue";
+import Registration from "@/components/Registration.vue";
+import axios from "axios";
+import {BASE_URL} from "@/baseUrl";
+
 export default {
     name: "SignInPage",
+    components: {Registration, Recovery, Auth},
     data() {
         return {
             isRecovery: false,
@@ -153,18 +44,11 @@ export default {
             this.isRegistration = !this.isRegistration;
             this.isRecovery = false
         },
-        onAuth() {
-            if (this.login === 'login' && this.password
-                === 'login') {
-                this.$store.commit('login', this.login)
-                this.$router.push('/lk')
-            }
         }
-    }
 }
 </script>
 
-<style scoped>
+<style>
 .content {
   background-image: url("/src/components/img/auth-background.jpg");
   background-size: cover;

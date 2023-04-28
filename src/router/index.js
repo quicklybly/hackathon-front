88d25650d01032457f65c3a40838c887cf273b83
danilkk <ezzfvkoko@gmail.com>
2023-04-28 11:53:13 +0300

    add tag datalist

--- src/router/index.js
+++ src/router/index.js
@@ -16,13 +16,13 @@ const routes = [
     component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
   },
   {
-    path: '/calendar',
-    name: 'calendar',
+    path: '/events',
+    name: 'events',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about"
-     */ '../views/./CalendarPage')
+     */ '../views/EventsPage.vue')
   },
   {
     path: '/auth',

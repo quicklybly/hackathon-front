4c2835b46d17e956c1861f93afefb977ebe6d89a
danilkk <ezzfvkoko@gmail.com>
2023-05-03 20:29:30 +0300

    transitions

--- src/components/EventPosts.vue
+++ src/components/EventPosts.vue
@@ -1,10 +1,12 @@
 <template>
-    <div
-        v-for="post in posts">
-        <event-post :post="post"
-        @update="this.$emit('update')"/>
-        <hr />
-    </div>
+    <transition-group name="post-list">
+        <div
+            v-for="post in posts">
+            <event-post :post="post"
+            @update="this.$emit('update')"/>
+            <hr />
+        </div>
+    </transition-group>
 </template>
 
 <script>
@@ -25,4 +27,21 @@ export default {
 
 <style scoped>
 
+.post-list event-post {
+    display: inline-block;
+    margin-right: 10px;
+}
+
+.post-list-enter-active, .post-list-leave-active {
+    transition: all 0.4s ease;
+}
+
+.post-list-enter-from, .post-list-leave-to {
+    opacity: 0;
+    transform: translateY(150px);
+}
+
+.post-list-move {
+    transition: transform 0.4s ease;
+}
 </style>
